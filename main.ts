bbef3cb2f91660bf0ff318bf1f13393cdde51443
Georg Graf (SPG) <grafg@spengergasse.at>
2025-10-26 23:38:52 +0100

    damits lustiger wird

--- main.ts
+++ main.ts
@@ -3,8 +3,11 @@ import { last_consecutive } from "./primerepo.ts";
 
 while (true) {
     const versuch = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+    console.log(`Testing number: ${versuch}`);
     if (await primeService.isPrime(versuch)) {
         console.log(`Found prime: ${versuch}`);
+    } else {
+        console.log(`Not prime: ${versuch}`);
     }
 }
 
