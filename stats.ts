76c92f95985467cb96d98da5f440a1fdf329d85d
Georg Graf (SPG) <grafg@spengergasse.at>
2025-10-29 13:25:43 +0100

    acp-commit

--- stats.ts
+++ stats.ts
@@ -1,19 +1,15 @@
 // import { primeService } from "./primeservice.ts";
 import * as repo from "./lib/primerepo.ts";
 
-console.log("Gathering statistics...");
 
 const lastConsecutive = await repo.last_consecutive();
-console.log(`Last consecutive prime in DB: ${lastConsecutive}`);
-
+console.log(`Last  consecutive prime  in DB: ${lastConsecutive}`);
 const countConsecutives = await repo.consecutives_count();
 console.log(`Total consecutive primes in DB: ${countConsecutives}`);
 
-const countAlones = await repo.alone_count();
-console.log(`Total alone primes in DB: ${countAlones}`);
-
-const biggestAlone = await repo.biggest_alone()!;
-console.log(`Biggest alone prime in DB: ${biggestAlone}`);
-
 const smallestAlone = await repo.smallest_alone()!;
 console.log(`Smallest alone prime in DB: ${smallestAlone}`);
+const biggestAlone = await repo.biggest_alone()!;
+console.log(`Biggest alone prime in DB: ${biggestAlone}`);
+const countAlones = await repo.alone_count();
+console.log(`Total alone primes in DB: ${countAlones}`);
